536af0fc8603832d877ded7f452a5a8c5efe9e5d
Andrii Hrytsenko <zandrr16@gmail.com>
2020-04-02 16:20:41 +0300

    Added AnimationController

---
+++ Assets/Editor/AnimalAnimationControllerEditor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+[CustomEditor(typeof(AnimalAnimationController))]
+public class AnimalAnimationControllerEditor : Editor
+{
+    bool fold;
+    public override void OnInspectorGUI()
+    {
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("sprites"));
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("gender"));
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("dir"));
+        fold = EditorGUILayout.BeginFoldoutHeaderGroup(fold,"spriteRenderers");
+        if(fold)
+        {
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("body"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("face"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("eyes"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("right_leg"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("left_leg"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("right_hand"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("left_hand"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("tail"));
+        }
+        serializedObject.ApplyModifiedProperties();
+    }
+}
---
+++ Assets/Scripts/AnimalAnimationController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalAnimationController : MonoBehaviour
+{
+    public AnimalSprites sprites;
+    [SerializeField] Gender gender;
+    [SerializeField] Direction dir;
+    Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        AnimalPack tmp;
+        switch (gender)
+        {
+            case Gender.male:
+                tmp = sprites.male;
+                break;
+            case Gender.female:
+                tmp = sprites.female;
+                break;
+            default:
+                tmp = sprites.child;
+                break;
+        }
+        switch(dir)
+        {
+            case Direction.up:
+                SetUpSprites(tmp.up);
+                break;
+            case Direction.down:
+                SetUpSprites(tmp.down);
+                break;
+            case Direction.side:
+                SetUpSprites(tmp.side);
+                break;
+        }
+
+        
+    }
+
+
+    #region Sprites SetUp
+    [SerializeField] SpriteRenderer body;
+    [SerializeField] SpriteRenderer face;
+    [SerializeField] SpriteRenderer eyes;
+    [SerializeField] SpriteRenderer right_leg;
+    [SerializeField] SpriteRenderer left_leg;
+    [SerializeField] SpriteRenderer right_hand;
+    [SerializeField] SpriteRenderer left_hand;
+    [SerializeField] SpriteRenderer tail;
+    void SetUpSprites(DirectionPack pack)
+    {
+        body.sprite = pack.body;
+        face.sprite = pack.face;
+        eyes.sprite = pack.eyes;
+        right_leg.sprite = pack.right_leg;
+        left_leg.sprite = pack.left_leg;
+        right_hand.sprite = pack.right_hand;
+        left_hand.sprite = pack.left_hand;
+        tail.sprite = pack.tail;
+    }
+    #endregion
+}
+public enum Gender
+{
+    male,
+    female,
+    child
+}
+public enum Direction
+{
+    up,
+    down,
+    side
+}
