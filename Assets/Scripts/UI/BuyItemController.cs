46270e0293fc5b7eb029b4939e92044bb49f05f3
Andrii Hrytsenko <zandrr16@gmail.com>
2020-04-14 15:04:21 +0300

    added items purchase

---
+++ Assets/Scripts/UI/BuyItemController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuyItemController : MonoBehaviour
+{
+    [SerializeField] GameObject[] items;
+
+    public void BuyItem(int index)
+    {
+        Instantiate(items[index], Vector3.zero, Quaternion.identity);
+    }
+}
